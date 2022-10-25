cfae918f42940995c2c7a8fb27903a9d6ce934cd
BhageshIIITD <bhagesh20558@iiitd.ac.in>
2022-10-25 16:47:11 +0530

    Dataset

---
+++ Scripts/get_dataset.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import opendatasets as od
+
+# Download the dataset
+
+
+os.chdir('../Datasets/')
+dataset = 'https://www.kaggle.com/datasets/rounakbanik/the-movies-dataset'
+od.download(dataset)
+os.chdir('../Scripts/')
